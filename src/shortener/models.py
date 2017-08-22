14e0beb2f3dba5f7f42a2879fe1d49bef2df88a3
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-22 11:43:23 +0100

    Handling Migration Failure - Test Machine

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -5,7 +5,12 @@ from django.db import models
 # Create your models here.
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220, )
-	shortcode = models.CharField(max_length=15, unique=True)
+	shortcode = models.CharField(max_length=15, unique=True) 
+	updated = models.DateTimeField(auto_now=True) #everytime the model is saved
+	timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+	#shortcode = models.CharField(max_length=15, null=True) Empty in databse is okay
+	#shortcode = models.CharField(max_length=15, default='chedefaultshortcode')
 
 	def __str__(self):
 		return str(self.url)
