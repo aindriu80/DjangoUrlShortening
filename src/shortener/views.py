f40304da4d7f7eb210f6e2ce3a7bffc608bb55c8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-08-28 11:51:05 +0100

    Rending New Templates and Context

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -23,35 +23,25 @@ class HomeView(View):
 
 	def post(self, request, *args, **kwargs):
 		form = SubmitUrlForm(request.POST)
-		if form.is_valid():
-			print(form.cleaned_data.get("url"))	
-
 		context = {
-		"title": "Submit URL",
-		"form": form
+			"title": "Submit URL",
+			"form": form
 		}
-		return render(request, "shortener/home.html", context) 
+		template = 	"shortener/home.html"
+		if form.is_valid():
+			new_url = form.cleaned_data.get("url")
+			obj, created = KirrURL.objects.get_or_create(url=new_url)
+			context = {
+			"object": obj,
+			"created": created,
+			}
+			if created:
+				template = "shortener/success.html"
+			else:
+				template = "shortener/already-exists.html"
 
-'''
-def test_view(request) :
-	return HttpResponse("some example")
+		return render(request, template, context) 
 
-
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV	
-	obj = get_object_or_404(KirrURL, shortcode=shortcode)	 	
-	# do soemthing
-	return HttpResponseRedirect(obj.url)
-
-
-class KirrCBView(View): # class based view
-	def get(self, request, shortcode=None, *args, **kwargs):		
-		obj = get_object_or_404(KirrURL, shortcode=shortcode)		
-		return HttpResponseRedirect(obj.url)
-
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV	
-	obj = get_object_or_404(KirrURL, shortcode=shortcode)	 
-	return HttpResponse("hello {sc}".format(sc=obj.url))
-'''
 
 class KirrCBView(View): # class based view
 	def get(self, request, shortcode=None, *args, **kwargs):		
@@ -59,31 +49,3 @@ class KirrCBView(View): # class based view
 		return HttpResponse("hello again {sc}".format(sc=shortcode))
 
 
-	#def post(self, request, *args, **kwargs):
-		#return HttpResponse()
-
-
-'''
-# Create your views here.
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV
-	#print(request.user)	
-	#print(request.user.is_authenticated())	
-	print('method is \n')
-	print(request.method)
-	
-	obj = get_object_or_404(KirrURL, shortcode=shortcode)
-	#obj_url = obj.url
-		
-	#obj = KirrURL.objects.get(shortcode=shortcode)
-	# try: 
-	# 	obj = KirrURL.objects.get(shortcode=shortcode)
-	# except:
-	# 	obj = KirrURL.objects.all().first()
-	 	
-	 
-	# obj_url = None
-	# qs = KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
-	# if qs.exists() and qs.count() == 1:
-	# 		obj = qs.first()
-	# 		obj_url = obj.url
-'''			 
